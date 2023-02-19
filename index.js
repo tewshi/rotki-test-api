7cb8779122dec2c93c66300a16ff64c2cdc1669e
Joseph Ojoko <entewshi@gmail.com>
2023-02-19 16:46:21 +0100

    configure for vercel

---
+++ index.js
@@ -0,0 +1,15 @@
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running: port', 3000)
+})
+
+// Export the Server API
+module.exports = server
